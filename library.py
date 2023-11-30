da41aba7f770c63f52f81666a157834a32eb7206
laurenengel78 <152507657+laurenengel78@users.noreply.github.com>
2023-11-30 09:04:12 -0500

    Add files via upload
    Initial commit

---
+++ library.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Lauren Engel
+# CSC 328, Fall 2023, Final Project
+# Library Functions
+
+import datetime
+import json
+import socket
+import sys
+
+# Description: Allows a socket to send a word packet containing a message
+# Parameters: socket sending_socket - socket sending the message
+#             string nickname - name of the client sending the message, or "server" if the server is sending the message
+#             string message - message to be sent
+# Return Value: None
+def send_message(sending_socket, nickname, message):
+
+    packet = make_word_packet(nickname, message)
+    sending_socket.sendall(packet)
+
+# Description: Allows a socket to read a word packet containing a message
+# Parameters: socket receiving_socket - socket reading the message
+#             bytes packet - word packet in bytes
+# Return Value: dict_packet - word packet as a dictionary
+def read_message(receiving_socket, packet):
+
+    json_packet = packet.decode()
+    dict_packet = json.loads(data)
+    return dict_packet
+#    bytes = b''
+#    while len(bytes) != length:
+#        curr_read = receiving_socket.recv(length - len(bytes))
+#        bytes += curr_read
+#        if len(curr_read) == 0: break
+#    return bytes
+
+# Description: Makes a word packet in a JSON format
+# Parameters: string nickname - name of the client sending the message, or "server" if the server is sending the message
+#             string message - message to be sent
+# Return Value: json_packet - json word packet in bytes with nickname, message, and length of message
+def make_word_packet(nickname, message):
+    packet = {"timestamp": datetime.now(), "nickname" : nickname, "message" : message}
+    json_packet = json.dump(packet)
+    return json_packet.encode()
