691a212d1e25ae3e345fc35f8d8033199cc56328
zandruchowitz <152507628+zandruchowitz@users.noreply.github.com>
2023-12-11 20:59:42 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -41,39 +41,33 @@ def main():
                 conn, _ = s.accept()
                 with conn:
                     #Send hello to connecting client
-                    conn.sendall(len("Hello, welcome to the server!").to_bytes(2, 'big') + "Hello".encode()) 
+                    conn.sendall(len("HELLO").to_bytes(2, 'big') + "HELLO".encode()) 
                     # conn.sendall(len("Enter a nickname").to_bytes(2, 'big') + x.encode())
                     # word_length = int.from_bytes(really_read(s, 2), 'big')
                     #   if word_length == 0: break
                     #nickname = really_read(s, word_length)                    
                     # for i in all_clients:
                     #   if i != nickname:
-                
-                    def createNickname(all_clients):
-                        '''
-                        Function name: createNickname
-                        Description: This function will create a unique nickname for the server
-                                      and return it to the main function
-                        Parameters: list_of_clients -- list of all current clients connected to server
-                        Return: nickname -- a unique nickname that will be added to the server
-                        '''
-                        
-                        conn.sendall(len("Enter a NICK").to_bytes(2, 'big') + "Enter a NICK".encode())
-                        word_length = int.from_bytes(really_read(conn, 2), 'big')
-                        nickname = really_read(conn, word_length)
-                        for i in all_clients:
-                            if i == nickname:
-                                return nickname
-                        conn.sendall(len("RETRY").to_bytes(2, 'big') + "RETRY".encode())
-                        createNickname(all_clients)  
 
-                    nickname = createNickname(all_clients)     
-                    all_clients.insert(nickname)
-                    conn.sendall(len("Welcome " + nickname).to_bytes(2, 'big') + ("Welcome " + nickname).encode())   
-                        #word = random.choice(WORDS)
-                        #word_packet = len(word).to_bytes(2, 'big') + word.encode()
-                        #conn.sendall(word_packet)
-                        #conn.sendall(random.choice(word_packets))
+                    unique = 0                
+                    while True:                        
+                        conn.sendall(len("NICK").to_bytes(2, 'big') + "NICK".encode())
+                        word_length = int.from_bytes(really_read(conn, 2), 'big')
+                        nickname = really_read(conn, word_length).decode()
+                        for i in all_clients:
+                            if i != nickname: 
+                                unique += 1                                     
+                        if unique == len(all_clients):
+                            conn.sendall(len("READY").to_bytes(2, 'big') + "READY".encode())
+                            break 
+                        conn.sendall(len("RETRY").to_bytes(2, 'big') + "RETRY".encode())
+
+                    all_clients.append(nickname)
+              
+
+                    
+
+ 
     except OSError as e:
         exit(e)
     except KeyboardInterrupt:
