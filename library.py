652de4cf1e1365f9cf87954a200bbb50426bbc47
laurenengel78 <152507657+laurenengel78@users.noreply.github.com>
2023-12-12 19:55:49 -0500

    Update library.py
    Added really_read function and edited read_message function to read correct number of bytes

--- library.py
+++ library.py
@@ -18,27 +18,36 @@ def send_message(sending_socket, nickname, message):
     packet = make_word_packet(nickname, message)
     sending_socket.sendall(packet)
 
+# Description: Reads all bytes of a word packet (taken from Dr. Schwesinger)
+# Parameters: s - socket to read from
+#             n - number of bytes to read
+# Return Value: bytes - bytes read
+def really_read(s, n):
+    bytes = b''
+    while len(bytes) != n:
+        curr_read = s.recv(n - len(bytes))
+        bytes += curr_read
+        if len(curr_read) == 0: break
+    return bytes
+
 # Description: Allows a socket to read a word packet containing a message
 # Parameters: socket receiving_socket - socket reading the message
 #             bytes packet - word packet in bytes
 # Return Value: dict_packet - word packet as a dictionary
-def read_message(receiving_socket, packet):
+def read_message(receiving_socket):
+    packet_length = int.from_bytes(really_read(receiving_socket, 2), 'big')
+    packet = really_read(receiving_socket, packet_length)
     json_packet = packet.decode()
     dict_packet = json.loads(data)
     return dict_packet
-#    bytes = b''
-#    while len(bytes) != length:
-#        curr_read = receiving_socket.recv(length - len(bytes))
-#        bytes += curr_read
-#        if len(curr_read) == 0: break
-#    return bytes
 
 # Description: Makes a word packet in a JSON format
 # Parameters: string nickname - name of the client sending the message, or "server" if the server is sending the message
 #             string message - message to be sent
-# Return Value: json_packet - json word packet in bytes with nickname, message, and length of message
+# Return Value: full_packet - length of packet in bytes and json word packet in bytes with nickname, message, and length of message
 def make_word_packet(nickname, message):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    packet = {"timestamp": timestamp, "nickname" : nickname, "message" : message}
-    json_packet = json.dumps(packet)
-    return json_packet.encode()
+    json_packet = json.dumps({"timestamp": timestamp, "nickname" : nickname, "message" : message})
+    length = len(json_packet.encode()).to_bytes(2, 'big')
+    full_packet = length + json_packet.encode()
+    return full_packet
